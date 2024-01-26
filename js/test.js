73831122ca4d2c5673c5494f8cf73f7965a6bc02
yuan.wencong <yuan.wencong@nint.com>
2024-01-26 11:13:07 +0800

    update

---
+++ js/test.js
@@ -0,0 +1,11 @@
+var a = 123;
+var b = 456;
+var c = 1111;
+var d = 22222;
+function add(num1, num2) {
+    return num1 + num2;
+}
+console.log(add(a, b));
+console.log("sdfjisji3ir");
+console.log("sdfjisji3ir");
+//# sourceMappingURL=test.js.map
---
+++ test.js
@@ -0,0 +1,7 @@
+var a = 123;
+var b = 456;
+function add(num1, num2) {
+    return num1 + num2;
+}
+// console.log(add(a, b));
+document.write("sdfjidsjf".toString());
