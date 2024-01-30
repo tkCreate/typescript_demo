888e38d5e174c770af83002965cf5def54ccaa34
yuan.wencong <yuan.wencong@nint.com>
2024-01-30 19:02:07 +0800

    update

--- js/test.js
+++ js/test.js
@@ -1,4 +1,20 @@
-// 导入指定模块
-import { greet } from './point.js';
-greet('Alice11'); // 调用导入的函数
+// 定义函数判断是否是正方形 Square, 返回boolean
+function isSquare(shape) {
+    return "size" in shape; // 如果返回true，说明是 Square
+}
+// 定义函数判断是否是长方形 Rectangle, 返回boolean
+function isRectangle(shape) {
+    return "width" in shape; // 如果返回true，说明是 Rectangle
+}
+function area(shape) {
+    // 如果是正方形
+    if (isSquare(shape))
+        return shape.size * shape.size;
+    // 如果是长方形
+    if (isRectangle(shape))
+        return shape.width * shape.height;
+}
+console.log(area({ size: 12 })); // 144
+let b = { width: 12, height: 2 };
+console.log(area(b)); // 24
 //# sourceMappingURL=test.js.map
--- test.ts
+++ test.ts
@@ -1,5 +1,34 @@
+type Square = {
+    size: number;
+};
 
-// 导入指定模块
-import {greet} from './point.js';
+type Rectangle = {
+    width: number;
+    height: number;
+};
 
-greet('Alice11'); // 调用导入的函数
+type Shape = Square | Rectangle;
+
+// 定义函数判断是否是正方形 Square, 返回boolean
+function isSquare(shape: Shape): shape is Square {
+    return "size" in shape; // 如果返回true，说明是 Square
+}
+
+// 定义函数判断是否是长方形 Rectangle, 返回boolean
+function isRectangle(shape: Shape): shape is Rectangle {
+    return "width" in shape;  // 如果返回true，说明是 Rectangle
+}
+
+function area(shape: Shape) {
+    // 如果是正方形
+    if (isSquare(shape)) return shape.size * shape.size;
+
+    // 如果是长方形
+    if (isRectangle(shape)) return shape.width * shape.height;
+
+}
+
+console.log(area({size: 12})); // 144
+
+let b: Rectangle = {width: 12, height: 2};
+console.log(area(b));  // 24
